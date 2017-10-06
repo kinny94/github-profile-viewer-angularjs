2092d1ded748bc64c22a32ab224a1284c9b11c14
Arjun  Dass <ad.sam007@gmail.com>
2017-10-05 20:43:57 -0500

    Added firest template

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { ProfileComponent } from './components/profile.component';
 @Component({
+
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
+        <profile></profile>
         <p>Hello World!</p>
-    `
+    `,
+    directives: [ProfileComponent]
 })
 export class AppComponent { }
